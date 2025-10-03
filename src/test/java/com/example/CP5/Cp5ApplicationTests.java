25cdcacc8b5ad744a9b7e4bc7b8c2660dd8a9d40
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-10-03 09:28:49 -0300

    Initial commit

---
+++ src/test/java/com/example/CP5/Cp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CP5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
